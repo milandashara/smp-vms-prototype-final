1702cb0568284241e891defc9096058fd406937d
milandashara <milandashara@gmail.com>
2014-12-15 17:36:23 +0800

    commit 1

--- public/app/js/filters.js
+++
@@ -1,5 +0,0 @@
-angular.module('smpvmsFilters', []).filter('checkmark', function () {
-    return function (input) {
-        return input ? '\u2713' : '\u2718';
-    };
-});
--- public/app/js/services.js
+++
@@ -1,8 +0,0 @@
-var smpvmsServices = angular.module('smpvmsServices', ['ngResource']);
-
-smpvmsServices.factory('Camera', ['$resource',
-    function ($resource) {
-        return $resource('/api/:id', {}, {
-            query: {method: 'GET', params: {id: 'cameras'}, isArray: true}
-        });
-    }]);
